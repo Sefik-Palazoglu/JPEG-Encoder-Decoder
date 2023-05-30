d6824b5b81448abf00642c41b9d1769f3e328957
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2023-05-30 15:31:03 +0300

    Add small error handling

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -7,6 +7,10 @@ Header* readJPG(const std::string& filename)
 {
     // Open file in input and binary format
     std::ifstream inFile = std::ifstream(filename, std::ios::in | std::ios::binary);
+    if (!inFile.is_open()) {
+        std::cout << "Error, input file cannot be opened --" << filename << "--\n";
+        return nullptr;
+    }
     return nullptr;
 }
 
