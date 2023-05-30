e94405c5ee63b0da5ecaf1c57d02292067ad5acd
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2023-05-30 19:28:48 +0300

    add DQT and COM support

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -6,11 +6,85 @@
 void readAPPN(std::ifstream& inFile, Header* header) {
     std::cout << "Reading APPN marker\n";
     uint length = ((inFile.get() << 8) + inFile.get());
+    std::cout << "length: " << (uint)length << '\n';
     
 
     // length which is 2 bytes is included in length
     for (int i = 0; i < length - 2; i++) {
         inFile.get();
+    }
+}
+
+void readQuantizationTable(std::ifstream& inFile, Header* header) {
+    std::cout << "Reading DQT marker\n";
+    // using in length, length should be signed
+    int length = ((inFile.get() << 8) + inFile.get());
+    length -= 2;
+
+    while (length > 0) {
+        byte tableInfo = inFile.get();
+        length -= 1;
+        byte tableID = tableInfo & 0x0F;
+        tableID = (tableInfo & 0x0F);
+
+        if (tableID > 3) {
+            std::cout << "Error - Table id cannot be greater than 3. tableID: " << (uint)tableID << "\n";
+            header->valid = false;
+            return;
+        }
+
+        header->quantizationTables[tableID].set = true;
+
+        // 0 -> 8 bits; 1 -> 16 bits
+        byte entrySize = ((tableInfo >> 4) & 0x0F);
+
+        if (entrySize == 0) {   // 8 bits per data
+            for (int i = 0; i < 64; i++) {
+                header->quantizationTables[tableID].table[zigZagMap[i]] = inFile.get();
+            }
+            length -= 64;
+        } else if (entrySize == 1) { // 16 bits per data
+            for (int i = 0; i < 64; i++) {
+                header->quantizationTables[tableID].table[zigZagMap[i]] = ((inFile.get() << 8) + inFile.get());
+            }
+            length -= 128;
+        }
+    }
+
+    if (length != 0) {
+        std::cout << "Error - invalid DQT Marker\n";
+        header->valid = false;
+        return;
+    }
+}
+
+void readComment(std::ifstream& inFile, Header* header) {
+    std::cout << "Reading COM marker\n";
+    uint length = ((inFile.get() << 8) + inFile.get());
+    std::cout << "length: " << (uint)length << '\n';
+
+    // length which is 2 bytes is included in length
+    for (int i = 0; i < length - 2; i++) {
+        inFile.get();
+    }
+}
+
+void printHeader(const Header* const header) {
+    if (header == nullptr) return;
+    if (header->valid == false) return;
+    std::cout << "DQT ====================\n";
+    for (int i = 0; i < 4; i++) {
+        if (header->quantizationTables[i].set) {
+            std::cout << "Table ID: " << i << "\n";
+            std::cout << "Table Data:";
+            for (int j = 0; j < 64; j++) {
+                if (j % 8 == 0) {
+                    std::cout << "\n";
+                }
+                std::cout << header->quantizationTables[i].table[j] << '\t';
+            }
+            std::cout << "\n";
+        }
     }
 }
 
@@ -57,9 +131,17 @@ Header* readJPG(const std::string& filename)
             return header;
         }
 
+        if (second == COM) {
+            readComment(inFile, header);
+        }
+
+        if (second == DQT) {
+            readQuantizationTable(inFile, header);
+            break;
+        }
+
         if (APP0 <= second && second <= APP15) {
             readAPPN(inFile, header);
-            break;
         }
 
         first = inFile.get();
@@ -87,6 +169,8 @@ int main(int argc, char** argv)
             delete header;
             continue;
         }
+
+        printHeader(header);
 
         // TODO: Decode Huffman Encoded Bitstream
 
--- src/jpg.h
+++ src/jpg.h
@@ -90,8 +90,25 @@ const byte JPG13 = 0xFD;
 const byte COM = 0xFE;
 const byte TEM = 0x01;
 
+struct QuantizationTable {
+    uint table[64] = { 0 };
+    bool set = false;
+};
+
 struct Header {
+    QuantizationTable quantizationTables[4];
     bool valid = true;
+};
+
+const byte zigZagMap[] = {
+    0,   1,  8, 16,  9,  2,  3, 10,
+    17, 24, 32, 25, 18, 11,  4,  5,
+    12, 19, 26, 33, 40, 48, 41, 34,
+    27, 20, 13,  6,  7, 14, 21, 28,
+    35, 42, 49, 56, 57, 50, 43, 36,
+    29, 22, 15, 23, 30, 37, 44, 51,
+    58, 59, 52, 45, 38, 31, 39, 46,
+    53, 60, 61, 54, 47, 55, 62, 63
 };
 
 #endif  // JPG_H
